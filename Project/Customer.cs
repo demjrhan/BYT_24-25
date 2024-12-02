14de7dab238539e7358a33555963c3a1133c95a5
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2024-12-02 16:52:37 +0100

    Several refactoring, extra validations, renaming, and added comments in necessary places to clarify changes.

--- Project/Customer.cs
+++ Project/Customer.cs
@@ -3,17 +3,22 @@
     public class Customer : Person
     {
         private static int _lastId = 0;
-        private static List<Customer> Instances = [];
+        public int CustomerId { get; private set; }
+        private static List<Customer> Instances = new List<Customer>();
+        public Membership? Membership { get; set; }
+        private DateTime _registerDate;
+        private Cart _cart = null!;
+
 
         //public static readonly string _verbose = "Customer";
         //public static readonly string _verbosePlural = "Customers";
-
-        private DateTime _registerDate;
-        private Cart _cart = null!;
         //Add connection with Membership class
-        private Membership? _membership;
 
-        public int CustomerId { get; private set; }
+        
+        
+        // Taking 1940 as imaginary establishing date of our company. Person can not register before that time. -Demirhan
+        private static readonly DateTime MinimumRegisterDate = new DateTime(1940, 1, 1);
+
         public DateTime RegisterDate
         {
             get => _registerDate;
@@ -21,9 +26,15 @@
             {
                 if (value > DateTime.Now)
                     throw new ArgumentException("Date cannot be set to a future time.", nameof(value));
+
+                if (value < MinimumRegisterDate)
+                    throw new ArgumentException($"Date cannot be earlier than {MinimumRegisterDate.ToShortDateString()}.", nameof(value));
+
                 _registerDate = value;
             }
         }
+
+
         public Cart Cart
         {
             get => _cart;
@@ -31,12 +42,18 @@
             {
                 if (value == null)
                     throw new ArgumentNullException(nameof(value), "Cart cannot be null.");
+                // Added extra logic in case of cart already assigned to this customer. -Demirhan
+                if (_cart == value)
+                    throw new InvalidOperationException("The same cart is already assigned.");
+               
                 if (value.CustomerId != CustomerId)
-                    throw new ArgumentException("Cart customer ID must match the Customer's ID.");
+                    throw new InvalidOperationException("This cart is already assigned to another customer.");
+
                 _cart = value;
             }
         }
-        public Membership? Membership { get; set; }
+
+
 
         public Customer(
             string name, string surname,
@@ -44,27 +61,36 @@
             string address, int age,
             bool isStudying, bool isWorking,
             bool isRetired, RetirementType? retirementType = null
-            ) : base(
-                name, surname,
-                email, phone,
-                address, age,
-                isStudying, isWorking,
-                isRetired, retirementType
-                )
+        ) : base(
+            name, surname,
+            email, phone,
+            address, age,
+            isStudying, isWorking,
+            isRetired, retirementType
+        )
         {
+            ValidateRetirement(isRetired, retirementType);
+            
             RegisterDate = DateTime.Now;
             CustomerId = _lastId++;
-
             Cart = new Cart(CustomerId);
-
             Instances.Add(this);
         }
+        
+        
+        // Checking the logic in retirementType to apply retirement. 
+        private static void ValidateRetirement(bool isRetired, RetirementType? retirementType)
+        {
+            if (!isRetired && retirementType != null)
+                throw new ArgumentException("RetirementType should be null if the customer is not retired.", nameof(retirementType));
+        }
+
 
         public Order? CreateOrder()
         {
-            if (Cart != null)
-                return Cart.ConvertToOrder();
-            return null;
+            /*Removed if Cart is null before converting the order.
+            Cart can not be null since we are creating and initializing in the constructor. We also check it in setter.-Demirhan */ 
+            return Cart.ConvertToOrder();
         }
 
         public Review CreateReview(int rating, string comment)
@@ -74,41 +100,47 @@
 
         public static void PrintInstances()
         {
-            foreach (var i in Instances)
+            foreach (var customer in Instances)
             {
-                Console.WriteLine(i.ToString());
+                Console.WriteLine(customer.ToString());
             }
         }
 
         public static bool Exists(int id)
         {
-            bool flag = false;
-            try
+            /*Code refactoring :  instead of checking by index in Instances list we are checking each instance one by one if its matching.
+            If so returning true otherwise false. -Demirhan */
+            foreach (var customer in Instances)
             {
-                Customer? o = Instances[id];
-                if (o != null) flag = true;
+                if (customer.CustomerId == id)
+                    return true;
             }
-            catch (IndexOutOfRangeException ex)
-            {
-                Console.Error.WriteLine(ex.Message);
-            }
-            return flag;
+            return false;
         }
 
+        
+        // Since we are checking already if customer exists in GetCustomerWithId method, there is no reason to second check. -Demirhan
+        // If customer does not exist we will face with error rather than 0. -Demirhan
         public static double GetDiscountPercentage(int customerId)
         {
-            Customer? customer = Instances.Find(x => (x.CustomerId == customerId));
-            if (customer != null)
-            {
-                return customer.GetDiscountPercentage();
-            }
-            return 0d;
+            Customer customer = GetCustomerWithId(customerId);
+            return customer.GetDiscountPercentage();
         }
+        
+
+        // Created getting customer with id method to apply reusability in code. -Demirhan
+        public static Customer GetCustomerWithId(int customerId)
+        {
+            Customer? customer = Instances.Find(x => x.CustomerId == customerId);
+            if (customer == null)
+                throw new InvalidOperationException($"Customer with ID {customerId} does not exist.");
+            return customer;
+        }
+
 
         public override string ToString()
         {
             return base.ToString() + " - Customer";
         }
     }
-
 }
